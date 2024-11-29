c0d8096656fd6150d24a26280c5a8e013173c397
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-11-29 18:00:57 +0100

    unix philosophy for ,create-bun-script

--- scripts/bun/src/create-bun-script.ts
+++ scripts/bun/src/create-bun-script.ts
@@ -1,19 +1,10 @@
-import {
-  Command,
-  CommandExecutor,
-  FileSystem,
-  Terminal,
-} from "@effect/platform";
+import { Command, CommandExecutor, FileSystem } from "@effect/platform";
 import { BunContext, BunRuntime } from "@effect/platform-bun";
-import { Effect, Schema } from "effect";
-import { editor } from "../interactiveShell.js";
+import { Console, Effect, Schema } from "effect";
 
 Effect.gen(function* () {
-  const terminal = yield* Terminal.Terminal;
-
-  const name = yield* Schema.NonEmptyString.pipe(
-    (s) => Schema.decode(s),
-    (d) => d(Bun.argv[2]),
+  const name = yield* Schema.NonEmptyString.pipe(Schema.decode, (d) =>
+    d(Bun.argv[2]),
   );
 
   const exec = yield* CommandExecutor.CommandExecutor;
@@ -21,14 +12,13 @@ Effect.gen(function* () {
   const scriptExists = yield* exec.exitCode(Command.make("which", name));
 
   if (scriptExists === 0) {
-    yield* terminal.display(`script ${name} already exists`);
-    return;
+    throw new Error(`script ${name} already exists`);
   }
 
   const fs = yield* FileSystem.FileSystem;
 
   const binFilePath = import.meta.dir + "/../bin/" + name;
-  const srcFilePath = import.meta.dir + "/../src/" + name + '.tsx'
+  const srcFilePath = import.meta.dir + "/" + name + ".tsx";
 
   yield* fs.writeFileString(
     binFilePath,
@@ -39,14 +29,11 @@ import "../src/${name}.tsx";
 
   yield* fs.writeFileString(
     srcFilePath,
-    `console.log("hello from ${srcFilePath}")
+    `console.log("Hello from ${srcFilePath}!")
 `,
   );
 
-  yield* editor(srcFilePath)
-
   yield* fs.chmod(binFilePath, 0o777);
 
-  yield* terminal.display(`created a bun script ${binFilePath}\n`);
-  yield* terminal.display(`created a bun src file ${srcFilePath}\n`);
+  yield* Console.log(srcFilePath); // pass this with pipe to e.g. and xargs vim
 }).pipe(Effect.provide(BunContext.layer), BunRuntime.runMain);

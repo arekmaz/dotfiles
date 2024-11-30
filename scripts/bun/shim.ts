7fa4dcbcf0449303793204a10063e65d1606ceae
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-11-30 23:18:07 +0100

    add single executable monolith build

---
+++ scripts/bun/shim.ts
@@ -0,0 +1,23 @@
+// enables building single executable
+// https://github.com/oven-sh/bun/issues/13522#issuecomment-2311173485
+const bin = Bun.file("dist/mb.js")
+let content = await new Response(bin).text()
+
+// Replace createRequire(import.meta.url) with require
+content = content.replace(/createRequire\(import\.meta\.url\)/g, "require")
+
+// Replace $(import.meta.url) where $ is "createRequire as something"
+const createRequireAsRegex = /(?<=createRequire as )(.+?)(\w*)(?=\})/g
+const matches = content.match(createRequireAsRegex)
+
+if (!matches?.length) {
+    throw new Error("No matches found")
+}
+
+for (const match of matches) {
+    const pattern = `${match}(import.meta.url)`
+      content = content.replaceAll(pattern, "require")
+}
+
+await Bun.write("dist/mb.js", content)
+console.log("Successfully shimmed bin/bin.js")
--- scripts/bun/src/monolith.ts
+++ scripts/bun/src/monolith.ts
@@ -1,16 +1,23 @@
-import { Command } from "@effect/cli"
-import { Console } from "effect"
-import { gpt } from "./gpt.js"
-import { tictactoe } from "./tictactoe.jsx"
-import { sb } from "./sb.js"
-import { cmt } from "./cmt.js"
+import { Command } from "@effect/cli";
+import { Console, Effect } from "effect";
+import { gpt } from "./gpt.js";
+import { tictactoe } from "./tictactoe.jsx";
+import { sb } from "./sb.js";
+import { cmt } from "./cmt.js";
+import { BunContext, BunRuntime } from "@effect/platform-bun";
+import { FetchHttpClient } from "@effect/platform";
 
-const command = Command.make(",m", {}, () =>
-  Console.log("Bun Monolith")
-).pipe(Command.withSubcommands([gpt, tictactoe, sb, cmt]))
+const command = Command.make(",m", {}, () => Console.log("Bun Monolith")).pipe(
+  Command.withSubcommands([gpt, tictactoe, sb, cmt]),
+);
 
-export const cli = Command.run(command, {
+const cli = Command.run(command, {
   name: "Bun Monolith CLI",
-  version: "v1.0.0"
-})
+  version: "v1.0.0",
+});
 
+cli(process.argv).pipe(
+  Effect.provide(BunContext.layer),
+  Effect.provide(FetchHttpClient.layer),
+  BunRuntime.runMain,
+);

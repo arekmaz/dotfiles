d5bf3d62bb3035e71fc2f74d665dee585ad61cd1
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2025-02-03 16:11:25 +0100

    remove nvim telescope and oil

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -68,23 +68,4 @@ require("lazy").setup({
       { "hrsh7th/cmp-nvim-lua" }, },
     },
   "djoshea/vim-autoread",
-  {
-    'stevearc/oil.nvim',
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {},
-    lazy = false,
-  },
-  {
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  }
 })
-
-require("oil").setup()
-
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>g', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>h', builtin.help_tags, { desc = 'Telescope help tags' })
